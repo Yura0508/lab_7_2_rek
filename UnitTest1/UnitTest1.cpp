f55802e34dd72d136588af54fe01ee13c36ee7b8
Yura <urijkisil2007@gmail.com>
2024-10-16 16:16:09 +0300

    f commit

---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,50 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../lab_7_2_rek/lab_7_2_rek.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+        TEST_METHOD(TestMinRow)
+        {
+            const int cols = 5;
+            int** a = new int* [1];
+            a[0] = new int[cols] { 5, 3, 8, 1, 4 };
+            int min = a[0][0];
+
+            MinRow(a, 0, cols, 0, min);
+            Assert::AreEqual(min, 1);
+
+            delete[] a[0];
+            delete[] a;
+        }
+
+        TEST_METHOD(TestMaxMinRows)
+        {
+            const int rows = 3;
+            const int cols = 4;
+            int** a = new int* [rows];
+
+
+            a[0] = new int[cols] { 1, 2, 3, 4 };
+            a[1] = new int[cols] { 9, 8, 7, 5 };
+            a[2] = new int[cols] { 5, 4, 6, 8 };
+
+            int max;
+
+            MaxMinRows(a, rows, cols, 0, max);
+            Assert::AreEqual(max, 5);
+
+            // Звільнення пам'яті
+            for (int i = 0; i < rows; i++)
+                delete[] a[i];
+            delete[] a;
+        }
+
+	};
+}
---
+++ lab_7_2_rek/lab_7_2_rek.cpp
@@ -0,0 +1,82 @@
+﻿// lab_7_2_rek.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <iomanip>
+#include <ctime>
+using namespace std;
+
+void Create(int** a, const int rowCount, const int colCount, const int Low, const int High, int i, int j);
+void Print(int** a, const int rowCount, const int colCount, int i);
+void MinRow(int** a, const int rowNo, const int colCount, int colNo, int& min);
+void MaxMinRows(int** a, const int rowCount, const int colCount, int rowNo, int& max);
+
+int main() {
+    srand(static_cast<unsigned>(time(nullptr)));
+    int Low = 1;
+    int High = 100;
+    int rows, cols;
+
+    cout << "Enter number of rows (k): ";
+    cin >> rows;
+    cout << "Enter number of columns (n): ";
+    cin >> cols;
+
+
+    int** a = new int* [rows];
+    for (int i = 0; i < rows; i++)
+        a[i] = new int[cols];
+
+    // Create the matrix
+    Create(a, rows, cols, Low, High, 0, 0);
+    cout << "Generated Matrix:" << endl;
+    Print(a, rows, cols, 0);
+
+
+    int max;
+    MaxMinRows(a, rows, cols, 0, max);
+    cout << "Largest of the minimum elements from each row = " << max << endl;
+
+
+    for (int i = 0; i < rows; i++)
+        delete[] a[i];
+    delete[] a;
+    cin.get();
+    return 0;
+}
+
+void Create(int** a, const int rowCount, const int colCount, const int Low, const int High, int i, int j) {
+    a[i][j] = Low + rand() % (High - Low + 1);
+    if (j < colCount - 1)
+        Create(a, rowCount, colCount, Low, High, i, j + 1);
+    else if (i < rowCount - 1)
+        Create(a, rowCount, colCount, Low, High, i + 1, 0);
+}
+
+void Print(int** a, const int rowCount, const int colCount, int i) {
+    if (i < rowCount) {
+        for (int j = 0; j < colCount; j++)
+            cout << setw(4) << a[i][j];
+        cout << endl;
+        Print(a, rowCount, colCount, i + 1);
+    }
+}
+
+void MinRow(int** a, const int rowNo, const int colCount, int colNo, int& min)
+{
+    if (a[rowNo][colNo] < min)
+        min = a[rowNo][colNo];
+    if (colNo < colCount - 1)
+        MinRow(a, rowNo, colCount, colNo + 1, min);
+}
+
+void MaxMinRows(int** a, const int rowCount, const int colCount, int rowNo, int& max) {
+    int min = a[rowNo][0];
+    MinRow(a, rowNo, colCount, 1, min);
+    if (rowNo == 0)
+        max = min;
+    if (min > max)
+        max = min;
+    if (rowNo < rowCount - 1)
+        MaxMinRows(a, rowCount, colCount, rowNo + 1, max);
+}
